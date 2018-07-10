0508c65c89d829795b86dc76c3f2ae85e823f470
heckenna <heckenna@rose-hulman.edu>
2018-07-10 17:00:21 -0400

    Refactor to Pokemon

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react';
-import { Route, NavLink,  Switch } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Route, NavLink, Switch } from 'react-router-dom'
 
-import './App.css';
-import Github from './Github';
-
-
+import './App.css'
+import Pokemon from './Pokemon'
+import Homework from './Homework'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <h3>
-            There aint no party like an API party.
-          </h3>
-          <h1>
-            API Party
-          </h1>
-          
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
         </header>
 
         <ul className="navLinks">
           <li>
-            <NavLink to="/github">GitHub API</NavLink>
+            <NavLink to="/pokemon">Pok&eacute; API</NavLink>
+          </li>
+          <li>
+            <NavLink to="/homework">Homework</NavLink>
           </li>
         </ul>
+
         <Switch>
-          <Route path="/github" Component={Githu}/>
+          <Route path="/pokemon" component={Pokemon} />
+          <Route path="/homework" component={Homework} />
           <Route render={() => (
-              <p> To get started, click one of the links above.</p>
-            )}/>
+            <p>To get started, click one of the links above.</p>
+          )}/>
         </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
