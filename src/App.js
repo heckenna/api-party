9a7d6a0c6d8a7c490c05ac7f940beae5d4d25095
heckenna <heckenna@rose-hulman.edu>
2018-07-10 15:38:13 -0400

    Baby's first routs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>
-          API Party
-        </h1>
+        <header>
+          <h3>
+            There aint no party like an API party.
+          </h3>
+          <h1>
+            API Party
+          </h1>
+          
+        </header>
 
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+        </ul>
+
+        <Route path="/github" render={() => <h3>GitHub!</h3>}/>
       </div>
     );
   }
